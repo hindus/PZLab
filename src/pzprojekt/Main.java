606bbc83ad7c8189137889d655d93d4ba1f380f7
hindus <mateuszhinca@gmail.com>
2014-01-15 13:02:17 +0100

    Reset

--- src/pzprojekt/Main.java
+++
@@ -1,8 +0,0 @@
-package pzprojekt;
-
-public class Main {
-
-	public static void main(){
-		System.out.println("Coś main ble ble");
-	}
-}
--- src/pzprojekt/StagingTest.java
+++
@@ -1,5 +0,0 @@
-package pzprojekt;
-
-public class StagingTest {
-
-}
