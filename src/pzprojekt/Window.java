d283152b346e4bc7940818ac5b51e6448b459fd6
wolfiek <krzysiek.kijo@gmail.com>
2014-01-16 09:06:08 +0100

    Panel logowania

--- src/pzprojekt/Window.java
+++ src/pzprojekt/Window.java
@@ -6,6 +6,7 @@ package pzprojekt;
  */
  
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -17,27 +18,113 @@ import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import javax.swing.JPasswordField;
+import java.awt.Insets;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
  
 public class Window extends JPanel {
+	private JTextField textField;
+	private JPasswordField passwordField;
     public Window() {
-        //super(new GridLayout(1, 1));
+        super(new GridLayout(1, 1));
          
         JTabbedPane tabbedPane = new JTabbedPane();
          
-        JComponent panel1 = makeTextPanel("Panel #1");
-        tabbedPane.addTab("EPER", panel1);
-        panel1.setPreferredSize(new Dimension(800, 500));
+        //tabbedPane.addTab("EPER", panel1);
+        JSplitPane pan1 = new JSplitPane();
+        JPanel pan1pan1 = new JPanel();
+        JPanel pan1pan2 = new JPanel();
+        pan1.setDividerLocation(250);
+        pan1.setLeftComponent(pan1pan1);
+        pan1.setRightComponent(pan1pan2);
+        tabbedPane.addTab("EPER", pan1);
+
+        //panel1.setPreferredSize(new Dimension(800, 500));
+//        JSplitPane pan1 = new JSplitPane();
+//        JPanel pan1pan1 = new JPanel();
+//        JPanel pan1pan2 = new JPanel();
+//        pan1.setLeftComponent(pan1pan1);
+//        pan1.setRightComponent(pan1pan2);
+        //panel1.add(pan1);
+
+
 
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
          
-        JComponent panel2 = makeTextPanel("Panel #2");
+        //JComponent panel2 = makeTextPanel("Panel #2");
+        JPanel panel2 = new JPanel();
         tabbedPane.addTab("Sklep", panel2);
         panel2.setPreferredSize(new Dimension(800, 500));
+        panel2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        
+        JPanel panel = new JPanel();
+        panel2.add(panel);
+        GridBagLayout gbl_panel = new GridBagLayout();
+        gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+        gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+        gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+        gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        panel.setLayout(gbl_panel);
+        
+        JLabel login = new JLabel("Nazwa użytkownika:");
+        GridBagConstraints gbc_login = new GridBagConstraints();
+        gbc_login.insets = new Insets(0, 0, 5, 5);
+        gbc_login.gridx = 0;
+        gbc_login.gridy = 1;
+        panel.add(login, gbc_login);
+        
+        textField = new JTextField();
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.insets = new Insets(0, 0, 5, 5);
+        gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField.gridx = 2;
+        gbc_textField.gridy = 1;
+        panel.add(textField, gbc_textField);
+        textField.setColumns(10);
+        JLabel password = new JLabel("Hasło:");
+        GridBagConstraints gbc_password = new GridBagConstraints();
+        gbc_password.anchor = GridBagConstraints.WEST;
+        gbc_password.insets = new Insets(0, 0, 5, 5);
+        gbc_password.gridx = 0;
+        gbc_password.gridy = 2;
+        panel.add(password, gbc_password);
+        
+        passwordField = new JPasswordField();
+        GridBagConstraints gbc_passwordField = new GridBagConstraints();
+        gbc_passwordField.insets = new Insets(0, 0, 5, 5);
+        gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_passwordField.gridx = 2;
+        gbc_passwordField.gridy = 2;
+        panel.add(passwordField, gbc_passwordField);
+        
+        JButton btnZarejestruj = new JButton("Zarejestruj");
+        GridBagConstraints gbc_btnZarejestruj = new GridBagConstraints();
+        gbc_btnZarejestruj.insets = new Insets(0, 0, 5, 5);
+        gbc_btnZarejestruj.gridx = 0;
+        gbc_btnZarejestruj.gridy = 3;
+        panel.add(btnZarejestruj, gbc_btnZarejestruj);
+        
+        JButton btnZaloguj = new JButton("Zaloguj");
+        GridBagConstraints gbc_btnZaloguj = new GridBagConstraints();
+        gbc_btnZaloguj.insets = new Insets(0, 0, 5, 5);
+        gbc_btnZaloguj.gridx = 2;
+        gbc_btnZaloguj.gridy = 3;
+        panel.add(btnZaloguj, gbc_btnZaloguj);
 
+        
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
          
         JComponent panel3 = makeTextPanel("Panel #3");
@@ -45,19 +132,24 @@ public class Window extends JPanel {
         panel3.setPreferredSize(new Dimension(800, 500));
 
         tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
-         
-        add(tabbedPane);
-        
-        
-        //chujowe drzewko
+//        //chujowe drzewko
         DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
         DefaultMutableTreeNode child1 = new DefaultMutableTreeNode("Child 1");
         root.add(child1);
         DefaultMutableTreeNode child2 = new DefaultMutableTreeNode("Child 2");
         root.add(child2);
+        pan1pan1.setLayout(null);
         JTree tree = new JTree(root);
-        panel1.add(new JScrollPane(tree));
-        //koniec chujowego drzewka
+        tree.setBounds(0, 0, 249, 406);
+        pan1pan1.add(tree);
+//        //koniec chujowego drzewka
+        add(tabbedPane);
+        
+        
+
+        
+
+        
         tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
     }
      
@@ -83,7 +175,7 @@ public class Window extends JPanel {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 500); 
         //Add content to the window.
-        frame.add(new Window(), BorderLayout.CENTER);
+        frame.getContentPane().add(new Window(), BorderLayout.CENTER);
          
         //Display the window.
         frame.pack();
