e01bca910efa94d99e90697ecf10979b7044d69a
wolfiek <krzysiek.kijo@gmail.com>
2014-01-16 09:29:57 +0100

    Przykladowe okienko do sklepu

--- src/pzprojekt/Window.java
+++ src/pzprojekt/Window.java
@@ -43,22 +43,13 @@ public class Window extends JPanel {
          
         JTabbedPane tabbedPane = new JTabbedPane();
          
-        //tabbedPane.addTab("EPER", panel1);
         JSplitPane pan1 = new JSplitPane();
         JPanel pan1pan1 = new JPanel();
         JPanel pan1pan2 = new JPanel();
-        pan1.setDividerLocation(250);
+        pan1.setDividerLocation(130);
         pan1.setLeftComponent(pan1pan1);
         pan1.setRightComponent(pan1pan2);
         tabbedPane.addTab("EPER", pan1);
-
-        //panel1.setPreferredSize(new Dimension(800, 500));
-//        JSplitPane pan1 = new JSplitPane();
-//        JPanel pan1pan1 = new JPanel();
-//        JPanel pan1pan2 = new JPanel();
-//        pan1.setLeftComponent(pan1pan1);
-//        pan1.setRightComponent(pan1pan2);
-        //panel1.add(pan1);
 
 
 
@@ -128,21 +119,16 @@ public class Window extends JPanel {
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
          
         JComponent panel3 = makeTextPanel("Panel #3");
-        tabbedPane.addTab("Autorzy", panel3);
+        tabbedPane.addTab("Przykładowe okienko sklepu", panel3);
         panel3.setPreferredSize(new Dimension(800, 500));
 
         tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
-//        //chujowe drzewko
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
-        DefaultMutableTreeNode child1 = new DefaultMutableTreeNode("Child 1");
-        root.add(child1);
-        DefaultMutableTreeNode child2 = new DefaultMutableTreeNode("Child 2");
-        root.add(child2);
+
         pan1pan1.setLayout(null);
-        JTree tree = new JTree(root);
-        tree.setBounds(0, 0, 249, 406);
+        
+        JTree tree = new JTree();
+        tree.setBounds(0, 0, 249, 427);
         pan1pan1.add(tree);
-//        //koniec chujowego drzewka
         add(tabbedPane);
         
         
@@ -155,10 +141,51 @@ public class Window extends JPanel {
      
     protected JComponent makeTextPanel(String text) {
         JPanel panel = new JPanel(false);
-        JLabel filler = new JLabel(text);
-        filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 1));
-        panel.add(filler);
+        panel.setLayout(null);
+        
+        JPanel panel_1 = new JPanel();
+        panel_1.setBounds(22, 31, 423, 215);
+        panel.add(panel_1);
+        GridBagLayout gbl_panel_1 = new GridBagLayout();
+        gbl_panel_1.columnWidths = new int[]{103, 147, 0, 0};
+        gbl_panel_1.rowHeights = new int[]{15, 0, 0, 0, 0};
+        gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        panel_1.setLayout(gbl_panel_1);
+        
+        JLabel label_1 = new JLabel("Zawartość Twojego koszyka:");
+        GridBagConstraints gbc_label_1 = new GridBagConstraints();
+        gbc_label_1.anchor = GridBagConstraints.WEST;
+        gbc_label_1.insets = new Insets(0, 0, 5, 5);
+        gbc_label_1.gridx = 0;
+        gbc_label_1.gridy = 1;
+        panel_1.add(label_1, gbc_label_1);
+        
+        JLabel lblNazwaPrzedmiotu = new JLabel("Nazwa przedmiotu");
+        GridBagConstraints gbc_lblNazwaPrzedmiotu = new GridBagConstraints();
+        gbc_lblNazwaPrzedmiotu.anchor = GridBagConstraints.WEST;
+        gbc_lblNazwaPrzedmiotu.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNazwaPrzedmiotu.gridx = 0;
+        gbc_lblNazwaPrzedmiotu.gridy = 2;
+        panel_1.add(lblNazwaPrzedmiotu, gbc_lblNazwaPrzedmiotu);
+        
+        JLabel lblSztuk = new JLabel("Sztuk");
+        GridBagConstraints gbc_lblSztuk = new GridBagConstraints();
+        gbc_lblSztuk.insets = new Insets(0, 0, 5, 5);
+        gbc_lblSztuk.gridx = 1;
+        gbc_lblSztuk.gridy = 2;
+        panel_1.add(lblSztuk, gbc_lblSztuk);
+        
+        JLabel lblNaleno = new JLabel("Należność");
+        GridBagConstraints gbc_lblNaleno = new GridBagConstraints();
+        gbc_lblNaleno.insets = new Insets(0, 0, 5, 0);
+        gbc_lblNaleno.gridx = 2;
+        gbc_lblNaleno.gridy = 2;
+        panel_1.add(lblNaleno, gbc_lblNaleno);
+        
+        JLabel label = new JLabel("Jesteś zalogowany jako:" /*(select name from users where id = idZalogowanego)*/);
+        label.setBounds(321, 4, 173, 15);
+        panel.add(label);
         return panel;
     }
      
