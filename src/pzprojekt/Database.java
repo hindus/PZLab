45eadfcd3c509ba0c0b3304240cf1b5a216824a8
hindus <mateuszhinca@gmail.com>
2014-01-22 12:46:17 +0100

    Baza danych

--- src/pzprojekt/Database.java
+++ src/pzprojekt/Database.java
@@ -1,5 +1,32 @@
 package pzprojekt;
 
+import java.sql.*;
+
 public class Database {
 
+	static Connection con;
+	static Statement st;
+	static ResultSet rs;
+	
+	public static void dbconnect()
+	{
+		try
+			{
+				String query="SELECT username FROM users WHERE password='haslo'";
+				String driver = "com.mysql.jdbc.Driver"; 
+				Class.forName(driver);
+				con = DriverManager.getConnection("jdbc:mysql://mysql.hostinger.pl/u365162046_pzlab","u365162046_pzlab","pzlabka");
+			} 
+		
+		catch (Exception ex)
+			{
+				;
+			}
+	}
+	
+	public static void userlogin(String username, String password)
+	{
+		
+	}
+	
 }
