2be0ed6bc4f5de1b1bf93b0bd0c16f149183d831
hindus <mateuszhinca@gmail.com>
2014-01-22 13:05:22 +0100

    Baza danych

--- src/pzprojekt/Database.java
+++ src/pzprojekt/Database.java
@@ -12,7 +12,6 @@ public class Database {
 	{
 		try
 			{
-				String query="SELECT username FROM users WHERE password='haslo'";
 				String driver = "com.mysql.jdbc.Driver"; 
 				Class.forName(driver);
 				con = DriverManager.getConnection("jdbc:mysql://mysql.hostinger.pl/u365162046_pzlab","u365162046_pzlab","pzlabka");
@@ -26,7 +25,20 @@ public class Database {
 	
 	public static void userlogin(String username, String password)
 	{
-		
+		try
+			{
+				String query="SELECT password FROM users WHERE username="+username;
+				st=con.createStatement();
+				rs=st.executeQuery(query);
+			
+				if(rs.getString(1).compareTo(password)==0) System.out.println("Zalogowano"); //zmienna zalogowany w klasie użytkownika w górę
+				st.close();
+			} 
+	
+		catch (Exception ex)
+			{
+				;
+			}
 	}
 	
 }
