9f6582e68a4dbff0a77629969e4b93ab7d2516e1
wolfiek <krzysiek.kijo@gmail.com>
2014-01-22 13:24:37 +0100

    dbconnect działa

--- src/pzprojekt/Database.java
+++ src/pzprojekt/Database.java
@@ -14,30 +14,67 @@ public class Database {
 			{
 				String driver = "com.mysql.jdbc.Driver"; 
 				Class.forName(driver);
-				con = DriverManager.getConnection("jdbc:mysql://mysql.hostinger.pl/u365162046_pzlab","u365162046_pzlab","pzlabka");
+				//dbc:mysql://localhost:3306/projekt","root","hasllo"
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projekt","root","moneta");
+				st = con.createStatement();
+
+
 			} 
 		
 		catch (Exception ex)
 			{
-				;
+			System.out.println("wedrujaca dupa");
+
 			}
+	}
+	
+	public static void main(String[] args)
+	{
+		 dbconnect();
+		 userlogin("krzychu","haslo");
 	}
 	
 	public static void userlogin(String username, String password)
 	{
 		try
 			{
-				String query="SELECT password FROM users WHERE username="+username;
-				st=con.createStatement();
+				//String query="SELECT password FROM client WHERE login="+username;
+				String query = "select login,password from client where login = '"+username+"'";
+
+				//st=con.createStatement();
 				rs=st.executeQuery(query);
+
 			
-				if(rs.getString(1).compareTo(password)==0) System.out.println("Zalogowano"); //zmienna zalogowany w klasie użytkownika w górę
+				//System.out.println(rs.getString("login"));
+				//if(rs.getString(1).compareTo(password)==0) System.out.println("Zalogowano");
+				//else System.out.println("Nie zalogowano"); //zmienna zalogowany w klasie użytkownika w górę
+				while(rs.next())
+				{
+					try
+					{
+							//int id = rs.getInt(1);
+							String login = rs.getString("login");
+							String password2 = rs.getString("PASSWORD");
+							System.out.println("Dane: Login: "+login+" Haslo: "+password2+"  --> zalogowano");
+							
+							//JOptionPane.showMessageDialog(null, "Uzytkownik odnaleziony! Dane: \n ID: "+id+" Imię: "+login+" haslo: "+password);
+
+					}
+					catch(SQLException ex)
+					{
+						System.out.println(ex);
+					}
+							//zlicz++;
+				}
+				
 				st.close();
 			} 
 	
 		catch (Exception ex)
 			{
-				;
+				ex.printStackTrace();
+
+
 			}
 	}
 	
